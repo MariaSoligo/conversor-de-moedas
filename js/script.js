1135a5f37b6a52269d322a6686457d5fc94cafc0
MariaSoligo <149611383+MariaSoligo@users.noreply.github.com>
2023-11-14 15:06:19 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!inputValue.value || inputValue.value < 0 ) {
+        alert('Informe um valor correto');
+        return;
+    }  else if(!selectedCurrency.value){
+        alert('Escolha uma moeda')
+        return;
+    }
+
+    converter();
+}
+
+function converter() {
+    if(selectedCurrency.value === 'eur') {
+        valueConverted = inputValue.value / 5.25;
+        result.innerHTML = valueFormatter('pt-BR', 'EUR')
+        animateResult();
+
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 4.91;
+        result.innerHTML = valueFormatter('en-US', 'USD')
+    }
+
+    animateResult();
+
+
+}
+
+function valueFormatter(locale, currency) {
+ const value = valueConverted.toLocaleString( `${locale}` , {style:'currency', currency: `${currency}` })
+return `<span>🪙</span> ${value} <span></span>`;
+}
+
+function animateResult() {
+    return result.animate([
+        {transform: 'translateY(-150px)'},
+        {transform: 'translateY(0px)'}
+    ], {duration:500})
+}
